57cfe1c05606763e04e90a67c6741076251558ea
dinnu93 <dinesh.padisetti@gmail.com>
2021-08-30 14:04:36 +0530

    Added stuff

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>JRE EPISODES LIST</title>
+</head>
+<body>
+	<h1 style="font-family: Verdana; text-align: center; margin-top: 150px;">JRE EPISODES LIST</h1>
+</body>
+</html>
