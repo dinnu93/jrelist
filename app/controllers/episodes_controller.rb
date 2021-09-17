65f2981bb2cd802d441b8c9b14f7c0f981895583
dinnu93 <dinesh.padisetti@gmail.com>
2021-09-17 12:15:29 +0530

    Added privacy policy

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -91,6 +91,9 @@ class EpisodesController < ApplicationController
     end
   end
 
+  def privacy_policy
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_episode
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "episodes#index"
   resources :episodes
+  get '/privacy_policy', to: 'episodes#privacy_policy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
