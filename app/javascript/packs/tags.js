77b1b12df2c837f1bca8999c82912fea11543661
dinnu93 <dinesh.padisetti@gmail.com>
2021-09-23 15:42:44 +0530

    Removed debug js info

--- app/javascript/packs/tags.js
+++ app/javascript/packs/tags.js
@@ -8,8 +8,7 @@ $(document).on('turbolinks:load', function(){
   .done(function(tag_data) {
     tags = tag_data;
     tag_names = tags.map(t => t.name);
-    console.log(tags);
-    console.log(tag_names);
+    
     options = {
       data: tag_names
     };
